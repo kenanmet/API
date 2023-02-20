94a1b3c3b572d164a4336afe2d28d01544f3abc9
Kenan MET <kenanmt.81@gmail.com>
2023-02-20 22:49:12 +0300

    ders5

---
+++ src/test/java/tests/C18_GetTestDataClassKullanimi.java
@@ -0,0 +1,24 @@
+package tests;
+
+public class C18_GetTestDataClassKullanimi {
+    /*
+        https://jsonplaceholder.typicode.com/posts/22 url'ine bir
+        GET request yolladigimizda donen response’in status kodunun 200
+        ve response body’sinin asagida verilen ile ayni oldugunutest ediniz
+            Response body :
+                {
+                "userId",3,
+                "id",22,
+                "title","dolor sint quo a velit explicabo quia nam",
+                "body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita ear
+                             um mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+                }
+    */
+
+    public void get01(){
+        // 1- URL hazirla
+        // 2- Expected Data hazirla
+        // 3- Responce'i kaydet
+        // 4- Assertion
+    }
+}
