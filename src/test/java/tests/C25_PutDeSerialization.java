5ced49474931493c1e81659c8b67611596ec196c
Kenan MET <kenanmt.81@gmail.com>
2023-02-25 14:24:10 +0300

    ders8

--- src/test/java/tests/C25_PutDeSerialization.java
+++ src/test/java/tests/C25_PutDeSerialization.java
@@ -1,8 +1,18 @@
 package tests;
 
+import baseURL.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
+import testData.TestDataJsonPlaceHolder;
 
-public class C25_PutDeSerialization {
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class C25_PutDeSerialization extends JsonPlaceHolderBaseUrl {
     /*
         https://jsonplaceholder.typicode.com/posts/70 url'ine asagidaki body’e sahip bir PUT request
         yolladigimizda donen response’in response body’sinin asagida verilen ile ayni oldugunu test ediniz
@@ -17,8 +27,28 @@ public class C25_PutDeSerialization {
     @Test
     public void put01(){
         // 1- URL ve body hazirla
+        specJsonPlace.pathParams("pp1","posts","pp2",70);
+
+        TestDataJsonPlaceHolder testDataJsonPlaceHolder = new TestDataJsonPlaceHolder();
+        HashMap<String,Object> reqBody = testDataJsonPlaceHolder.requestBodyOlusturMap();
+
         // 2- Expected Data hazirla
+        HashMap<String,Object> expData = testDataJsonPlaceHolder.requestBodyOlusturMap();
+
         // 3- Responce'i kaydet
+        Response response = given().
+                                    spec(specJsonPlace).
+                                    contentType(ContentType.JSON).
+                            when().
+                                    body(reqBody).
+                                    put("/{pp1}/{pp2}");
+
         // 4- Assertion
+        HashMap<String,Object>resMap = response.as(HashMap.class);
+
+        Assert.assertEquals(expData.get("title"),resMap.get("title"));
+        Assert.assertEquals(expData.get("body"),resMap.get("body"));
+        Assert.assertEquals(expData.get("userId"),resMap.get("userId"));
+        Assert.assertEquals(expData.get("id"),resMap.get("id"));
     }
 }
--- src/test/java/tests/C26_GetDeSerialization.java
+++ src/test/java/tests/C26_GetDeSerialization.java
@@ -1,8 +1,17 @@
 package tests;
 
+import baseURL.DummyBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+import testData.TestDataDummy;
 
-public class C26_GetDeSerialization {
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class C26_GetDeSerialization extends DummyBaseUrl {
     /*
         http://dummy.restapiexample.com/api/v1/employee/3 url’ine bir GET request
         gonderdigimizde donen response’un asagidaki gibi oldugunu test edin.
@@ -21,9 +30,25 @@ public class C26_GetDeSerialization {
     */
     @Test
     public void get01(){
-        // 1- URL ve body hazirla
+        // 1- URL hazirla
+        specDummy.pathParams("pp1","employee","pp2",3);
+
         // 2- Expected Data hazirla
+        TestDataDummy testDataDummy = new TestDataDummy();
+        HashMap<String,Object> expData = testDataDummy.createExpectedBodyMap();
+
         // 3- Responce'i kaydet
+        Response response = given().spec(specDummy).when().get("/{pp1}/{pp2}");
+
         // 4- Assertion
+        HashMap<String,Object> resMap = response.as(HashMap.class);
+
+        Assert.assertEquals(expData.get("status"),resMap.get("status"));
+        Assert.assertEquals(expData.get("message"),resMap.get("message"));
+        Assert.assertEquals(((Map)(expData.get("data"))).get("id"),((Map)(resMap.get("data"))).get("id"));
+        Assert.assertEquals(((Map)(expData.get("data"))).get("employee_name"),((Map)(resMap.get("data"))).get("employee_name"));
+        Assert.assertEquals(((Map)(expData.get("data"))).get("employee_salary"),((Map)(resMap.get("data"))).get("employee_salary"));
+        Assert.assertEquals(((Map)(expData.get("data"))).get("employee_age"),((Map)(resMap.get("data"))).get("employee_age"));
+        Assert.assertEquals(((Map)(expData.get("data"))).get("profile_image"),((Map)(resMap.get("data"))).get("profile_image"));
     }
 }
